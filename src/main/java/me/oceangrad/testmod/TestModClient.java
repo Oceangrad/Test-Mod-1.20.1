b060a63b2f7e22d42a898c50fc3d2e0653aad992
Oceangrad <peterportal112@gmail.com>
2023-12-26 20:24:24 +0300

    Added simple InfestedBlock

--- src/main/java/me/oceangrad/testmod/TestModClient.java
+++ src/main/java/me/oceangrad/testmod/TestModClient.java
@@ -1,10 +1,14 @@
 package me.oceangrad.testmod;
 
+import me.oceangrad.testmod.blocks.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.scoreboard.ScoreboardCriterion;
 
 public class TestModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.INFESTED_TEST_BLOCK, RenderLayer.getCutout());
     }
 }
--- src/main/java/me/oceangrad/testmod/blocks/ModBlocks.java
+++ src/main/java/me/oceangrad/testmod/blocks/ModBlocks.java
@@ -5,21 +5,23 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.LeavesBlock;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
     public static final Block CORAL_LEAVES_BLOCK = registerBlock("coral_leaves", new LeavesBlock(
         FabricBlockSettings.copyOf(Blocks.ACACIA_LEAVES)
     ));
-
+    public static final Block INFESTED_TEST_BLOCK = registerBlock("infested_test_block", new InfestedBlock(
+        CORAL_LEAVES_BLOCK,
+        FabricBlockSettings.copyOf(Blocks.ACACIA_LEAVES)
+    ));
     public static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, new Identifier(TestMod.MOD_ID, name), block);
@@ -30,15 +32,16 @@ public class ModBlocks {
 
     private static void addBlockItemsToFunctionalBlockItemGroup(FabricItemGroupEntries entries){
         entries.add(CORAL_LEAVES_BLOCK.asItem());
+        entries.add(INFESTED_TEST_BLOCK.asItem());
     }
     private static void addBlockItemsToNaturalBlockItemGroup(FabricItemGroupEntries entries){
         entries.add(CORAL_LEAVES_BLOCK.asItem());
+        entries.add(INFESTED_TEST_BLOCK.asItem());
     }
 
     public static void onModBlocksRegister(){
         TestMod.LOGGER.info(String.format("Started registering mod blocks for %s", TestMod.MOD_ID));
 
-        // Code
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(ModBlocks::addBlockItemsToFunctionalBlockItemGroup);
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(ModBlocks::addBlockItemsToNaturalBlockItemGroup);
 
